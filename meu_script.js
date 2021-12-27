e828bf24a3028051823734c63ed3b03ef3220d7c
francisneyferreira <francisneyferreira@brasilcard.net>
2021-12-27 10:18:31 -0300

    primeito commit js

---
+++ meu_script.js
@@ -0,0 +1,6 @@
+//alert('Olá, seja bem vindo ao curso!')
+document.getElementById('nome').value = 'Oi' //método que recupera elemento dentro do DOM
+
+//comentário js de uma linha
+
+/* comentário js de múltiplas linhas */
