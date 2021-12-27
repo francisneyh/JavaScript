3714bdcfb2a0d49fc4c5bfac3d53d1efb859bca6
francisneyferreira <francisneyferreira@brasilcard.net>
2021-12-27 16:15:21 -0300

    variaveis

--- meu_script.js
+++ meu_script.js
@@ -1,4 +1,9 @@
 //alert('Olá, seja bem vindo ao curso!')
+
+/*
+    selecionar um elemento do DOM
+    atualizar o valor desse elemento com uma string
+*/
 document.getElementById('nome').value = 'Oi' //método que recupera elemento dentro do DOM
 
 //comentário js de uma linha
